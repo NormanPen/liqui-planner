8d385200d1d8d839c50473c4dbefe60b27240467
Norman Pendzich <norman.pendzich@outlook.de>
2022-04-20 16:38:12 +0200

    2

---
+++ src/js/main.js
@@ -0,0 +1,53 @@
+"use strict";
+
+let einnahmen = 0,
+  ausgaben = 0,
+  bilanz = 0,
+  titel,
+  typ,
+  betrag,
+  datum;
+
+const eintrag_erfassen = function () {
+  titel = prompt("titel:");
+  typ = prompt("Typ (Einnahmen oder Ausgaben):");
+  betrag = parseInt(prompt("Betrag in Cent:"));
+  datum = prompt("Datum (jjjj-mm-tt:");
+};
+
+const eintrag_ausgeben = function (titel, typ, betrag, datum) {
+  console.log(`Titel: ${titel}
+Typ: ${typ}
+Betrag: ${betrag}
+Datum: ${datum}
+`);
+};
+
+const eintrag_mitgesamtbilanz_verrechnen = function (typ, betrag) {
+  if (typ === "Einnahmen") {
+    einnahmen = einnahmen + betrag;
+    bilanz = bilanz + betrag;
+  } else if (typ === "Ausgaben") {
+    ausgaben = ausgaben - betrag;
+    bilanz = bilanz - betrag;
+  } else {
+    console.log(`Der Typ "${typ}" ist nicht bekannt`);
+  }
+};
+
+const gesamtbilanz_ausgeben = function (einnahmen, ausgaben, bilanz) {
+  console.log(`
+Einnahmen: ${einnahmen} ct 
+Ausgaben: ${ausgaben} ct
+Bilanz: ${bilanz} ct
+Bilanz ist positiv: ${bilanz >= 0}`);
+};
+
+const eintrag_hinzufuegen = function () {
+  eintrag_erfassen();
+  eintrag_ausgeben(titel, typ, betrag, datum);
+  eintrag_mitgesamtbilanz_verrechnen(typ, betrag);
+  gesamtbilanz_ausgeben(einnahmen, ausgaben, bilanz);
+};
+eintrag_hinzufuegen();
+eintrag_hinzufuegen();
